c1b0f05dea5a11adee838644684db5d83fe15dc9
Vladimir Kežić <squall.p@gmail.com>
2023-07-30 20:10:08 +0200

    undo test
    server difference causing problems

--- app/Http/Controllers/vendor/Chatify/MessagesController.php
+++ app/Http/Controllers/vendor/Chatify/MessagesController.php
@@ -135,8 +135,7 @@ class MessagesController extends Controller
             $message = Chatify::newMessage([
                 'from_id' => Auth::user()->id,
                 'to_id' => $request['id'],
-                'body' => htmlentities(trim($request['message']), ENT_QUOTES, 'UTF-8'),
-                'body_cripted' => Crypt::encrypt(htmlentities(trim($request['message']), ENT_QUOTES, 'UTF-8')),
+                'body' => Crypt::encrypt(htmlentities(trim($request['message']), ENT_QUOTES, 'UTF-8')),
                 'attachment' => ($attachment) ? json_encode((object)[
                     'new_name' => $attachment,
                     'old_name' => htmlentities(trim($attachment_title), ENT_QUOTES, 'UTF-8'),
