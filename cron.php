873b72835e634620b27d0a7fa3faffe7ea3c2d1f
Vladimir Kežić <squall.p@gmail.com>
2023-08-03 17:00:59 +0200

    Cron adjustemnt

--- cron.php
+++ cron.php
@@ -1,4 +1,4 @@
 <?php
-$shelloutput = shell_exec('php artisan schedule:run >> /dev/null 2>&1');
+$shelloutput = shell_exec("php artisan schedule:run >> /dev/null 2>&1");
 echo "<pre>$shelloutput</pre>";
 ?>
