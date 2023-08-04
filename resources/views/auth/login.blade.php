62467ee0136d262642b7f971444de42e51485877
Vladimir Kežić <squall.p@gmail.com>
2023-08-04 14:57:01 +0200

    Info message added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,13 @@
 
         <form id="captcha-form" method="POST" action="{{ route('login') }}">
             @csrf
-
+            <div>
+                ChatBuntu is an anonymous messenger with cyfered temporary kept communication.<br />
+                That means you don't need to register with real mail adress or to own one. All your messeges(except images) are cyripted and kept for a short time. Unseen messages are kept for a day and seen messages are kept for 6 hours. 
+                You have possbility to delete your conversation at all time and it would be unaccesible to anyone. 
+                <br />
+                <br />
+            </div>
             <!-- Email Address -->
             <div>
                 <x-label for="email" :value="__('Email')" />
